05280ef6a955485b942ee90d31e3d4b2b6f959bf
Chris-Totu <cris-hod@hotmail.com>
2023-01-31 13:58:23 +0100

    demo-1

---
+++ src/app/demos/demo1/demo1.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo1-hello-world',
+  templateUrl: './demo1.component.html',
+  styleUrls: ['./demo1.component.scss']
+})
+export class Demo1Component {
+  private _firstName: string;
+  private _lastName: string;
+  isDisabled: boolean = false;
+
+  constructor(){
+    this._firstName = "John";
+    this._lastName = "Doe";
+  }
+  //Getters
+  public get firstName(): string {
+    return this._firstName;
+  }
+
+  public get lastName() : string {
+    return this._lastName; 
+  }
+  
+  
+}
